be7c89c4ae44cc32cc222b2bf8caee6bfc7176f2
Zac Morris <morriszt75@gmail.com>
2022-12-18 11:49:19 -0500

    Changed back

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1779,7 +1779,6 @@
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  */
 //#define FILAMENT_RUNOUT_SENSOR
-//  Wrote this to test ssh signing
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define FIL_RUNOUT_ENABLED_DEFAULT true // Enable the sensor on startup. Override with M412 followed by M500.
   #define NUM_RUNOUT_SENSORS   1          // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
