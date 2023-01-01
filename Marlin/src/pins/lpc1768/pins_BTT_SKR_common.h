af7759b8afeaf763a9d1439827123ade9f2c51e9
Zac Morris <morriszt75@gmail.com>
2023-01-01 14:56:25 -0500

    changed fan pin

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_common.h
@@ -92,15 +92,11 @@
 #ifndef HEATER_0_PIN
   #define HEATER_0_PIN                     P2_07
 #endif
-#if HOTENDS == 1 && DISABLED(HEATERS_PARALLEL)
-  #ifndef FAN1_PIN
-    #define FAN1_PIN                       P2_04
-  #endif
-#else
-  #ifndef HEATER_1_PIN
-    #define HEATER_1_PIN                   P2_04
-  #endif
+// Defined the fan pin myself
+#ifndef FAN1_PIN
+  #define FAN1_PIN                       P2_04
 #endif
+
 #ifndef FAN_PIN
   #define FAN_PIN                          P2_03
 #endif
