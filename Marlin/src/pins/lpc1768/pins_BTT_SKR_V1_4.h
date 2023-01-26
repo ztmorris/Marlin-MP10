06408b225a099170bfdfc3f3c72ffec22626cf9f
Zac Morris <morriszt75@gmail.com>
2023-01-26 12:46:58 -0500

    MP10 Settings debugged

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -129,7 +129,7 @@
 // Z Probe (when not Z_MIN_PIN)
 //
 #ifndef Z_MIN_PROBE_PIN
-  #define Z_MIN_PROBE_PIN                  P0_10
+  // #define Z_MIN_PROBE_PIN                  P0_10
   #define Z_MIN_PROBE_PIN                  P1_27
 #endif
 
