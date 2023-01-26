d9ceef95e66a6b9017a94626943f65b42350938e
Zac Morris <morriszt75@gmail.com>
2023-01-26 18:14:31 -0500

    updated pins and temperatures

--- Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
+++ Marlin/src/pins/lpc1768/pins_BTT_SKR_V1_4.h
@@ -72,43 +72,43 @@
 #ifdef X_STALL_SENSITIVITY
   #define X_STOP_PIN                  X_DIAG_PIN
   #if X_HOME_TO_MIN
-    #define X_MAX_PIN                      P1_26  // E0DET
+    #define X_MAX_PIN                      P1_29  // E0DET
   #else
-    #define X_MIN_PIN                      P1_26  // E0DET
+    #define X_MIN_PIN                      P1_29  // E0DET
   #endif
 #elif ENABLED(X_DUAL_ENDSTOPS)
   #ifndef X_MIN_PIN
-    #define X_MIN_PIN                      P1_26  // X-STOP
+    #define X_MIN_PIN                      P1_29  // X-STOP
   #endif
   #ifndef X_MAX_PIN
-    #define X_MAX_PIN                      P1_26  // E0DET
+    #define X_MAX_PIN                      P1_29  // E0DET
   #endif
 #else
-  #define X_STOP_PIN                       P1_26  // X-STOP
+  #define X_STOP_PIN                       P1_29  // X-STOP
 #endif
 
 #ifdef Y_STALL_SENSITIVITY
   #define Y_STOP_PIN                  Y_DIAG_PIN
   #if Y_HOME_TO_MIN
-    #define Y_MAX_PIN                      P1_25  // E1DET
+    #define Y_MAX_PIN                      P1_28  // E1DET
   #else
-    #define Y_MIN_PIN                      P1_25  // E1DET
+    #define Y_MIN_PIN                      P1_28  // E1DET
   #endif
 #elif ENABLED(Y_DUAL_ENDSTOPS)
   #ifndef Y_MIN_PIN
-    #define Y_MIN_PIN                      P1_25  // Y-STOP
+    #define Y_MIN_PIN                      P1_28  // Y-STOP
   #endif
   #ifndef Y_MAX_PIN
-    #define Y_MAX_PIN                      P1_25  // E1DET
+    #define Y_MAX_PIN                      P1_28  // E1DET
   #endif
 #else
-  #define Y_STOP_PIN                       P1_25  // Y-STOP
+  #define Y_STOP_PIN                       P1_28  // Y-STOP
 #endif
 
 #ifdef Z_STALL_SENSITIVITY
   #define Z_STOP_PIN                  Z_DIAG_PIN
   #if Z_HOME_TO_MIN
-    #define Z_MAX_PIN                      P1_00  // PWRDET
+    #define Z_MAX_PIN                      P1_27  // PWRDET
   #else
     #define Z_MIN_PIN                      P1_27  // PWRDET
   #endif
@@ -117,7 +117,7 @@
     #define Z_MIN_PIN                      P1_27  // Z-STOP
   #endif
   #ifndef Z_MAX_PIN
-    #define Z_MAX_PIN                      P1_00  // PWRDET
+    #define Z_MAX_PIN                      P1_27  // PWRDET
   #endif
 #else
   #ifndef Z_STOP_PIN
@@ -129,8 +129,6 @@
 // Z Probe (when not Z_MIN_PIN)
 //
 #ifndef Z_MIN_PROBE_PIN
-  // #define Z_MIN_PROBE_PIN                  P1_25
-  // #define Z_MIN_PROBE_PIN                  P1_27
   #define Z_MIN_PROBE_PIN                  P1_27
 #endif
 
